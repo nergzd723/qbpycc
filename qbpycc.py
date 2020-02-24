42bcbc17be5ef761992c12bd89a7aaa4c3de3117
Mark <53225591+nergzd723@users.noreply.github.com>
2020-02-24 19:04:32 +0200

    add comments

--- qbpycc.py
+++ qbpycc.py
@@ -10,9 +10,17 @@ class Nasmsheet:
         if '"' not in keyword:
             for letter in keyword:
                 if letter.isdigit():
-                    #probably a statementon line "+line+" line
+                    #probably has something to deal with math
                     keyword1 = keyword[keyword.index(letter):]
+                    # Look, imagine we have PRINT 5+10
+                    # Parsing it could be very difficult and also not efficient
+                    # Temporary solution is just doing calculation in Python shell
+                    # So, we just do self.printstring = *insert calculations here*
+                    # But it won`t work in any other way
+                    # Need parser very much
                     exec("self.printstring = str({})".format(keyword1))
+                    # We already have number how much times has PRINT called
+                    # So we increase it by 1, do printstring*insertnumberhere*: db "*insert value here*", 10
                     self.data = self.data + "printstring{}: ".format(str(self.mctr+1))+'db "{}", 10\n'.format(self.printstring)
                     self.mctr += 1
                     self.text = self.text + "mov rax, 1\nmov rdi, 1\nmov rsi, {}\nmov rdx, {}\nsyscall\n".format("printstring"+str(self.mctr), len(self.printstring)+1)
@@ -20,21 +28,34 @@ class Nasmsheet:
                     return
         for letter in keyword:
             if letter == '"' or letter == "'":
+                # Found "!
+                # Cutting string before "!
                 keyword1 = keyword[keyword.index(letter):]
+                # Then searching for another one " or '
                 for lette in keyword1:
                     if lette == '"' or lette == "'":
+                        # Found second "!
+                        # slicing string
                         keyword1 = keyword1[keyword1.index(letter)+1:]
                         keyword1 = keyword1[:-1]
+                        # Got it, break from cycle
                         self.printstring = keyword1
                         break
         for letter in keyword:
+            # Huh, it looks like we have ; symbol
+            # It means that after that symbol it is an variable OR EXPRESSION!
             if letter == ";" and (keyword[keyword.index(letter)-1]  == '"' or keyword[keyword.index(letter)-1]  == "'"):
+                # If we have it in internal buffer, just add it to string!
                 vari = keyword[keyword.index(letter):]
                 if vari in self.var:
                     self.printstring = self.printstring + self.varcontent[self.var.index(vari)]
-    
+                # Honestly, think that that internal buffer wasn't a good idea
+                # Need parser EVEN MORE
+        # doing the same thing as earlier
         self.data = self.data + "printstring{}: ".format(str(self.mctr+1))+'db "{}", 10\n'.format(self.printstring)
         self.mctr += 1
+        # To perform a PRINT syscall we need rax set to OUTPUT, rdi set to STDOUT, rsi set to adress of string, and rdx set to lenght of it.
+        # Just insert it here
         self.text = self.text + "mov rax, 1\nmov rdi, 1\nmov rsi, {}\nmov rdx, {}\nsyscall\n".format("printstring"+str(self.mctr), len(self.printstring)+1)
         self.printstring = ""
     def export(self):
@@ -53,6 +74,7 @@ class Nasmsheet:
             try:
                 self.interpret(word)
             except:
+                # If we even hit an internal Python error, we should quit, can't proceed further, needs bugfix
                 raise CCerr("general compiler error", word)
     def __init__(self, content, isFullcc):
         self.content = content
@@ -67,7 +89,13 @@ class Nasmsheet:
         self.varb = ""
     def interpret(self, keyword):
         if keyword[:3] == "REM" or keyword[:1] == "'":
+            # REM or ' are comments
             keyword1 = keyword[1:]
+            # This is a tricky thing. Python don't know is it a REM or a '
+            # We can cutoff first symbol immedeately
+            # I've done stupid thing there, if second letter is E than assume it's a REM
+            # But if we encounter something like 'EEEEE it will cutoff some characters
+            # Bugfix is pretty easy though
             if keyword1[0] == "E":
                 keyword1 = keyword1[2:]
             self.text = self.text + ";"+keyword1+"\n"
@@ -78,20 +106,29 @@ class Nasmsheet:
             keyword1 = keyword[5:]
             for letter in keyword1:
                 if letter == ";":
+                    # Find the variable to store input
                     keyword1w = keyword1.split(";")[0]
                     keyword1var = keyword1.split(";")[1]
+                    # Call printk
                     self.printk(keyword1w)
-                    self.bss = self.bss + keyword1var+": resb 1024\n" # for now only 255 B
-                    self.text = self.text + "mov rax, 0\nmov rdi, 0\nmov rsi, {}\nmov rdx, 255\nsyscall\n".format(keyword1var)
+                    # Reserve 1024B for the buffer
+                    self.bss = self.bss + keyword1var+": resb 1024\n" # for now only 1024 B
+                    # To perform a INPUT, we need rax set to INPUT, rdi set to STDIN, rsi set to variable, rdx set to size in bytes
+                    self.text = self.text + "mov rax, 0\nmov rdi, 0\nmov rsi, {}\nmov rdx, 1024\nsyscall\n".format(keyword1var)
         elif keyword == "END":
             if self.fcc:
                 self.text = self.text + ";;;;;;;;;;;;;;;;;;;;\n;{}\n;;;;;;;;;;;;;;;;;;;;\n".format(keyword)
+                # To end the program, we need RAX set to 60, rdi set to 0
             self.text = self.text + "mov rax, 60\nxor rdi, rdi\nsyscall\n"
         elif keyword == "CLS":
+            # CLS reason in all QBASIC programs is clearing the screen
+            # But we can't clear STDOUT
+            # So just skip it!
             if self.fcc:
                 self.text = self.text + ";;;;;;;;;;;;;;;;;;;;\n;{}\n;;;;;;;;;;;;;;;;;;;;\n".format(keyword)
             return
         elif keyword[:5] == "PRINT":
+            # Already explained
             varflag = False
             if self.fcc:
                 self.text = self.text + ";;;;;;;;;;;;;;;;;;;;\n;{}\n;;;;;;;;;;;;;;;;;;;;\n".format(keyword)
@@ -138,11 +175,18 @@ class Nasmsheet:
         elif keyword[:3] == "LET":
             if self.fcc:
                 self.text = self.text + ";;;;;;;;;;;;;;;;;;;;\n;{}\n;;;;;;;;;;;;;;;;;;;;\n".format(keyword)
+            # First, cutoff these LET characters
             keyword1 = keyword[3:]
+            # Then, remove ANY whitespaces
             keyword1 = keyword1.replace(" ", "")
+            # Split by "="
             varname = keyword1.split("=")[0]
             varc = keyword1.split("=")[1]
+            # varname should be a name of variable
+            # varcontent - it's content
+            # write values to DATA sections
             self.data = self.data + varname+ ': db "{}", 10\n'.format(varc)
+            # However, USE internal buffer too
             self.var.append(varname)
             self.varcontent.append(varc)
         else:
